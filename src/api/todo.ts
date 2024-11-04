a3bd03cb03e470b0d588911dbe412dbe516e5bf5
ChaeYubin <cyoobin@naver.com>
2024-11-04 21:22:21 +0900

    feat: 투두 관련 api 로직 추가

---
+++ src/api/todo.ts
@@ -0,0 +1,82 @@
+import { DeleteTodoParams, UpdateTodoParams } from './../types/todo';
+import axiosInstance from '../libs/axiosInstance';
+import { CreateTodoParams, TodoItem } from '@/types/todo';
+
+export const getTodos = async (token: string): Promise<TodoItem[] | Error> => {
+  try {
+    const response = await axiosInstance.get('/todos', {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return response.data.data;
+  } catch (err: any) {
+    return new Error(err.response?.data?.details || '투두리스트 조회 오류가 발생했습니다.');
+  }
+};
+
+export const getTodoById = async (token: string, id: string): Promise<TodoItem | Error> => {
+  try {
+    const response = await axiosInstance.get(`/todos/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return response.data.data;
+  } catch (err: any) {
+    return new Error(err.response?.data?.details || '투두리스트 조회 오류가 발생했습니다.');
+  }
+};
+
+export const createTodo = async ({ title, content, token }: CreateTodoParams): Promise<TodoItem | Error> => {
+  try {
+    const response = await axiosInstance.post(
+      '/todos',
+      { title, content },
+      {
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+    return response.data.data;
+  } catch (err: any) {
+    return new Error(err.response?.data?.details || '투두 생성 오류가 발생했습니다.');
+  }
+};
+
+export const updateTodo = async ({ title, content, id, token }: UpdateTodoParams): Promise<TodoItem | Error> => {
+  try {
+    const response = await axiosInstance.put(
+      '/todos',
+      { title, content, id },
+      {
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+    return response.data.data;
+  } catch (err: any) {
+    return new Error(err.response?.data?.details || '투두 수정 오류가 발생했습니다.');
+  }
+};
+
+export const deleteTodo = async ({ id, token }: DeleteTodoParams): Promise<{ data: null } | Error> => {
+  try {
+    const response = await axiosInstance.post(
+      '/todos',
+      { id },
+      {
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+    return response.data.data;
+  } catch (err: any) {
+    return new Error(err.response?.data?.details || '투두 삭제 오류가 발생했습니다.');
+  }
+};
---
+++ src/types/todo.ts
@@ -0,0 +1,11 @@
+export interface TodoItem {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type CreateTodoParams = Pick<TodoItem, 'title' | 'content'> & { token: string };
+export type UpdateTodoParams = Pick<TodoItem, 'title' | 'content' | 'id'> & { token: string };
+export type DeleteTodoParams = Pick<TodoItem, 'id'> & { token: string };
